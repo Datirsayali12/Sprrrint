5ba106d54c57c934a55692bff2a813efd4fde3e9
Datirsayali12 <sayalidatir70@gmail.com>
2024-03-05 00:13:52 +0530

    database code

--- Sprrrint/core/settings.py
+++ Sprrrint/core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devsearch',
+        'USER':'postgres',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
