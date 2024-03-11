98f7e3ba42ab7edfbaceb01168f27132e39b76c8
Datirsayali12 <sayalidatir70@gmail.com>
2024-03-11 11:03:45 +0530

    account added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=255)
+    user_image = models.URLField()
+    is_pro = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+    
+class BillingPersonalInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=100)
+    address1 = models.TextField()
+    address2 = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=100)
+    postal_code = models.IntegerField()
+    
+class UserSelection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profession = models.CharField(max_length=100)
+    preference = models.CharField(max_length=100)
+    experience = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Selection for User: {self.user.username}"
